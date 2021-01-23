049ad20f42c4bcbae20e73f0e01d4ac2f5a52429
Nicholas Maltbie <nick.dmalt@gmail.com>
2021-01-23 16:41:04 -0500

    Updated player input to lock on certian screens
    Added player ability to lock movement state or cursor depending on the screen loaded
    This allows for easily configurable screens taht the player can switch between and have the
    cursor and player react properly depending on what information is being shown.

--- Assets/Scripts/UI/MenuController.cs
+++ Assets/Scripts/UI/MenuController.cs
@@ -1,4 +1,6 @@
-﻿using UnityEngine;
+﻿using System.Collections.Generic;
+using PropHunt.Utils;
+using UnityEngine;
 
 namespace PropHunt.UI
 {
@@ -7,6 +9,43 @@ namespace PropHunt.UI
     /// </summary>
     public class MenuController : MonoBehaviour
     {
+        /// <summary>
+        /// Serialize container to change screen whenever a player gives an input
+        /// </summary>
+        [System.Serializable]
+        public class InputScreenChange
+        {
+            /// <summary>
+            /// Input to listen to from the player, listen to button down event
+            /// </summary>
+            public string input;
+            /// <summary>
+            /// Screen to load when the player presses this input as a button down
+            /// </summary>
+            public GameObject menu;
+        }
+
+        /// <summary>
+        /// Input screen change events for this menu
+        /// </summary>
+        public List<InputScreenChange> screenChangeInputs = new List<InputScreenChange>();
+
+        /// <summary>
+        /// Unity service for parsing player inputs
+        /// </summary>
+        public IUnityService unityService = new UnityService();
+
+        public void Update()
+        {
+            foreach(InputScreenChange change in this.screenChangeInputs)
+            {
+                if (unityService.GetButtonDown(change.input))
+                {
+                    SetScreen(change.menu);
+                }
+            }
+        }
+
         /// <summary>
         /// Request a new screen using a prefab name
         /// </summary>
