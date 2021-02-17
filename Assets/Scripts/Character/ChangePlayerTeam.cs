d0754af2cee270011f3872a02b1ad796d9f94599
Nicholas Maltbie <nick.dmalt@gmail.com>
2021-02-17 11:07:49 -0500

    Added team swap tests and perfabs for the basic test scene

--- Assets/Scripts/Character/ChangePlayerTeam.cs
+++ Assets/Scripts/Character/ChangePlayerTeam.cs
@@ -1,23 +1,36 @@
 ﻿using Mirror;
+using PropHunt.Utils;
 using UnityEngine;
 
 namespace PropHunt.Character
 {
     public class ChangePlayerTeam : NetworkBehaviour
     {
+
+        public INetworkService networkService;
         public string setTeam;
         public GameObject newPrefab;
+
+        public void Start()
+        {
+            this.networkService = new NetworkService(this);
+            if (!ClientScene.prefabs.ContainsValue(newPrefab))
+            {
+                ClientScene.RegisterPrefab(newPrefab);
+            }
+        }
 
         public void OnTriggerEnter(Collider other)
         {
             var team = other.GetComponent<PlayerTeam>();
-            if (isServer && team != null && team.playerTeam != setTeam)
+            var networkIdentity = other.GetComponent<NetworkIdentity>();
+            if (this.networkService.isServer && team != null && team.playerTeam != setTeam && networkIdentity.connectionToClient != null)
             {
-                NetworkConnection conn = other.GetComponent<NetworkIdentity>().connectionToClient;
+                NetworkConnection conn = networkIdentity.connectionToClient;
                 GameObject oldPlayer = other.gameObject;
                 GameObject newPlayer = Instantiate(newPrefab);
-                NetworkServer.ReplacePlayerForConnection(conn, newPlayer);
                 newPlayer.GetComponent<PlayerTeam>().playerTeam = setTeam;
+                NetworkServer.ReplacePlayerForConnection(conn, newPlayer);
                 NetworkServer.Destroy(oldPlayer);
             }
         }
