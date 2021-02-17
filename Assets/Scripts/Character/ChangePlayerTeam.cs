7e3ad887be3800a7969c14b5bbc5092cdfcba0e2
Nicholas Maltbie <nick.dmalt@gmail.com>
2021-02-16 20:01:32 -0500

    Added ability to change between teams

--- Assets/Scripts/Character/ChangePlayerTeam.cs
+++ Assets/Scripts/Character/ChangePlayerTeam.cs
@@ -1,11 +1,25 @@
-﻿using System.Collections;
-using System.Collections.Generic;
+﻿using Mirror;
 using UnityEngine;
 
-namespace PropHunt.Player
+namespace PropHunt.Character
 {
-    public class ChangePlayerTeam : MonoBehaviour
+    public class ChangePlayerTeam : NetworkBehaviour
     {
-        
+        public string setTeam;
+        public GameObject newPrefab;
+
+        public void OnTriggerEnter(Collider other)
+        {
+            var team = other.GetComponent<PlayerTeam>();
+            if (isServer && team != null && team.playerTeam != setTeam)
+            {
+                NetworkConnection conn = other.GetComponent<NetworkIdentity>().connectionToClient;
+                GameObject oldPlayer = other.gameObject;
+                GameObject newPlayer = Instantiate(newPrefab);
+                NetworkServer.ReplacePlayerForConnection(conn, newPlayer);
+                newPlayer.GetComponent<PlayerTeam>().playerTeam = setTeam;
+                NetworkServer.Destroy(oldPlayer);
+            }
+        }
     }
 }
