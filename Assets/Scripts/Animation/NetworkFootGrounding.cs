a7c572580773cdca957d7f0e11a85767fa3686c2
Nicholas Maltbie <nick.dmalt@gmail.com>
2021-04-12 23:15:58 -0400

    Added newtork control for player foot grounding

---
+++ Assets/Scripts/Animation/NetworkFootGrounding.cs
@@ -0,0 +1,80 @@
+
+using Mirror;
+using PropHunt.Character;
+using PropHunt.Utils;
+using UnityEngine;
+
+namespace PropHunt.Animation
+{
+    /// <summary>
+    /// Allow for player food grounding synchronization over network
+    /// </summary>
+    [RequireComponent(typeof(NetworkIKControl))]
+    public class NetworkFootGrounding : NetworkBehaviour
+    {
+        [SyncVar(hook = nameof(OnGroundFeetChange))]
+        public bool groundFeet;
+
+        /// <summary>
+        /// component to control player foot grounding
+        /// </summary>
+        public PlayerFootGrounded playerFootGrounded;
+
+        /// <summary>
+        /// Network IK control for synchronizing IK information
+        /// </summary>
+        private NetworkIKControl networkIKControl;
+
+        /// <summary>
+        /// Network service for operating character control
+        /// </summary>
+        public INetworkService networkService;
+
+        public void OnGroundFeetChange(bool _, bool newState)
+        {
+            playerFootGrounded.enableFootGrounded = newState;
+        }
+
+        public void SetFootGroundedState(bool newState)
+        {
+            if (!isServer)
+            {
+                CmdSetFootGroundedState(newState);
+            }
+            else
+            {
+                groundFeet = newState;
+                playerFootGrounded.enableFootGrounded = newState;
+            }
+
+            if (isServer || isLocalPlayer)
+            {
+                // Change from newtorked control to local control
+                // Grounding feet is done locally so invert network state
+                networkIKControl.SetIKGoalState(AvatarIKGoal.LeftFoot, !newState);
+                networkIKControl.SetIKGoalState(AvatarIKGoal.RightFoot, !newState);
+            }
+        }
+
+        public void Awake()
+        {
+            this.networkService = new NetworkService(this);
+
+            this.networkIKControl = GetComponent<NetworkIKControl>();
+        }
+
+        public void Start()
+        {
+            if (networkService.isServer)
+            {
+                SetFootGroundedState(true);
+            }
+        }
+
+        [Command]
+        public void CmdSetFootGroundedState(bool newState)
+        {
+            SetFootGroundedState(newState);
+        }
+    }
+}
