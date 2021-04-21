15a92a5a1fcc37cf49a87915ae1743b0cc8f5afb
Nicholas Maltbie <nick.dmalt@gmail.com>
2021-04-21 12:34:30 -0400

    Updated soem settings to create default scroll bar

---
+++ Assets/Scripts/UI/ChatBoxAutoScroll.cs
@@ -0,0 +1,14 @@
+using UnityEngine;
+using UnityEngine.UI;
+
+namespace PropHunt.UI
+{
+    public class ChatBoxAutoScroll : MonoBehaviour
+    {
+
+        public void Update()
+        {
+            // myText.line
+        }
+    }
+}
