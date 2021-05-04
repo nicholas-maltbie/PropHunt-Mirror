e8886a53be3526fad7066e55dc3a6c2221a2f1e1
Nicholas Maltbie <nick.dmalt@gmail.com>
2021-05-04 18:09:54 -0400

    Added sound effect on hit mono behaviour

---
+++ Assets/Scripts/Environment/SoundEffectOnHit.cs
@@ -0,0 +1,11 @@
+using UnityEngine;
+
+namespace PropHunt.Environment
+{
+    /// <summary>
+    /// Have an object generate sound effects when it hits something
+    /// </summary>
+    public abstract class SoundEffectOnHit : MonoBehaviour
+    {
+    }
+}
