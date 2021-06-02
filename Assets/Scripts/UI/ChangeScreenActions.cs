05e1f9d9b588cd1b2450b394de5e9c8615d6ec5a
Nicholas Maltbie <nick.dmalt@gmail.com>
2021-06-02 17:38:40 -0400

    Cleaned up some existing code

--- Assets/Scripts/UI/ChangeScreenActions.cs
+++ Assets/Scripts/UI/ChangeScreenActions.cs
@@ -1,3 +1,5 @@
+using System.Collections.Generic;
+using System.Linq;
 using System.Text.RegularExpressions;
 using UnityEngine;
 using UnityEngine.UI;
@@ -6,59 +8,98 @@ namespace PropHunt.UI.Actions
 {
     public class ChangeScreenActions : MonoBehaviour
     {
+
         /// <summary>
-        /// Dropdown events should be organized as
-        ///  0 - Fullscreen
-        ///  1 - Windowed
-        ///  2 - Borderless Fullscreen
+        /// Dropdown with windowed/fullscreen/borderless options
         /// </summary>
         public Dropdown windowedDropdown;
 
+        /// <summary>
+        /// Options to set screen resolution
+        /// </summary>
         public Dropdown resolutionDropdown;
+
+        /// <summary>
+        /// Display being rendered
+        /// </summary>
+        public static Display currentDisplay = Display.main;
 
         public const string resolutionParser = @"(\d+)[^\d](\d+)(\s*\*\s*(\d+)\s*)?";
 
-        public void SetScreenResolution()
+        public const string fullscreenModeName = "Fullscreen";
+        public const string windowedModeName = "Windowed";
+        public const string borderlessWindowModeName = "Borderless Windowed";
+
+        public static readonly string[] windowedDropdownText = { fullscreenModeName, windowedModeName, borderlessWindowModeName };
+        public static readonly string[] resolutionDropdownText = {
+            "1920×1080 * 60",
+            "1920×1080 * 30",
+            "1600×900",
+            "1366×768",
+            "1280×720",
+            "1024×576"
+        };
+
+        public static List<Dropdown.OptionData> MakeDropdownList(string[] optionsText) => new List<string>(optionsText).Select(
+            text => new Dropdown.OptionData(text)).ToList<Dropdown.OptionData>();
+
+        public FullScreenMode GetFullScreenMode(string selectedMode)
         {
-            string selectedResolution = resolutionDropdown.options[resolutionDropdown.value].text.Trim();
-
-            Match match = Regex.Match(selectedResolution, resolutionParser);
-
-            int resX = int.Parse(match.Groups[1].Value);
-            int resY = int.Parse(match.Groups[2].Value);
-            if (int.TryParse(match.Groups[4].Value, out int update))
+            switch (selectedMode)
             {
-                Screen.SetResolution(resX, resY, Screen.fullScreenMode, update);
-                UnityEngine.Debug.Log($"Setting screen resolution to {resX}x{resY} with update {update}");
-            }
-            else
-            {
-                Screen.SetResolution(resX, resY, Screen.fullScreenMode);
-                UnityEngine.Debug.Log($"Setting screen resolution to {resX}x{resY}");
+                case fullscreenModeName:
+                    return FullScreenMode.ExclusiveFullScreen;
+                case windowedModeName:
+                    return FullScreenMode.Windowed;
+                case borderlessWindowModeName:
+                    return FullScreenMode.FullScreenWindow;
+                default:
+                    return FullScreenMode.ExclusiveFullScreen;
             }
         }
 
-        public void SetFullScreenMode()
+        public void Start()
         {
-            FullScreenMode mode = FullScreenMode.ExclusiveFullScreen;
-            string selectedMode = windowedDropdown.options[windowedDropdown.value].text.Trim();
-            if (selectedMode == "Fullscreen")
+            windowedDropdown.ClearOptions();
+            windowedDropdown.AddOptions(MakeDropdownList(windowedDropdownText));
+            windowedDropdown.onValueChanged.AddListener(delegate
             {
-                mode = FullScreenMode.ExclusiveFullScreen;
-            }
-            else if (selectedMode == "Windowed")
+                UpdateDisplayInfo();
+            });
+
+            resolutionDropdown.ClearOptions();
+            resolutionDropdown.AddOptions(MakeDropdownList(resolutionDropdownText));
+            resolutionDropdown.onValueChanged.AddListener(delegate
             {
-                mode = FullScreenMode.Windowed;
-            }
-            else if (selectedMode == "Borderless Fullscreen")
+                UpdateDisplayInfo();
+            });
+        }
+
+        public void UpdateDisplayInfo()
+        {
+            string selectedResolution = resolutionDropdown.options[resolutionDropdown.value].text.Trim();
+            FullScreenMode mode = GetFullScreenMode(windowedDropdown.options[windowedDropdown.value].text.Trim());
+
+            Match resolutionParse = Regex.Match(selectedResolution, resolutionParser);
+
+            int resX = int.Parse(resolutionParse.Groups[1].Value);
+            int resY = int.Parse(resolutionParse.Groups[2].Value);
+            if (int.TryParse(resolutionParse.Groups[4].Value, out int update))
             {
-                mode = FullScreenMode.FullScreenWindow;
-            }
 #if UNITY_EDITOR
-            UnityEngine.Debug.Log("Setting screen mode " + mode.ToString());
+                UnityEngine.Debug.Log($"Setting {mode} with resolution {resX}x{resY} and update {update}");
 #else
-            Screen.fullScreenMode = mode;
+                Screen.SetResolution(resX, resY, mode, update);
 #endif
+            }
+            else
+            {
+#if UNITY_EDITOR
+                UnityEngine.Debug.Log($"Setting {mode} with resolution {resX}x{resY}");
+#else
+                Screen.SetResolution(resX, resY, mode);
+#endif
+            }
         }
     }
 }
