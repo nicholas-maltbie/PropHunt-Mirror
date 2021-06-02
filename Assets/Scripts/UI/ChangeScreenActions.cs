90dc84fe97b586b0065f6021b5e632f5ba242c92
Nicholas Maltbie <nick.dmalt@gmail.com>
2021-06-02 18:17:10 -0400

    reworked how the screen change actions work so it can persist between executions

--- Assets/Scripts/UI/ChangeScreenActions.cs
+++ Assets/Scripts/UI/ChangeScreenActions.cs
@@ -1,3 +1,4 @@
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text.RegularExpressions;
@@ -8,6 +9,10 @@ namespace PropHunt.UI.Actions
 {
     public class ChangeScreenActions : MonoBehaviour
     {
+        private const string resolutionWidthPlayerPrefKey = "ResolutionWidth";
+        private const string resolutionHeightPlayerPrefKey = "ResolutionHeight";
+        private const string resolutionRefreshRatePlayerPrefKey = "RefreshRate";
+        private const string fullScreenPlayerPrefKey = "FullScreen";
 
         /// <summary>
         /// Dropdown with windowed/fullscreen/borderless options
@@ -24,24 +29,31 @@ namespace PropHunt.UI.Actions
         /// </summary>
         public static Display currentDisplay = Display.main;
 
-        public const string resolutionParser = @"(\d+)[^\d](\d+)(\s*\*\s*(\d+)\s*)?";
-
         public const string fullscreenModeName = "Fullscreen";
         public const string windowedModeName = "Windowed";
         public const string borderlessWindowModeName = "Borderless Windowed";
 
-        public static readonly string[] windowedDropdownText = { fullscreenModeName, windowedModeName, borderlessWindowModeName };
-        public static readonly string[] resolutionDropdownText = {
-            "1920×1080 * 60",
-            "1920×1080 * 30",
-            "1600×900",
-            "1366×768",
-            "1280×720",
-            "1024×576"
-        };
+        private FullScreenMode currentFullscreen;
+        private Resolution currentResolution;
+        private Resolution[] resolutions;
 
-        public static List<Dropdown.OptionData> MakeDropdownList(string[] optionsText) => new List<string>(optionsText).Select(
-            text => new Dropdown.OptionData(text)).ToList<Dropdown.OptionData>();
+        public static readonly List<string> windowedDropdownText = new List<string>(
+            new string[] { fullscreenModeName, windowedModeName, borderlessWindowModeName });
+
+        public int GetFullScreenModeDropdownIndex(FullScreenMode mode)
+        {
+            switch (mode)
+            {
+                case FullScreenMode.ExclusiveFullScreen:
+                    return windowedDropdownText.IndexOf(fullscreenModeName);
+                case FullScreenMode.Windowed:
+                    return windowedDropdownText.IndexOf(windowedModeName);
+                case FullScreenMode.FullScreenWindow:
+                    return windowedDropdownText.IndexOf(borderlessWindowModeName);
+                default:
+                    return 0;
+            }
+        }
 
         public FullScreenMode GetFullScreenMode(string selectedMode)
         {
@@ -60,46 +72,77 @@ namespace PropHunt.UI.Actions
 
         public void Start()
         {
+            this.resolutions = Screen.resolutions.OrderBy(i => new Tuple<int, int>(-i.width, -i.height)).ToArray();
+            LoadSettings();
+
+            SetupFullscreenDropdown();
+            SetupResolutionDropdown();
+        }
+
+        private void SetupFullscreenDropdown()
+        {
             windowedDropdown.ClearOptions();
-            windowedDropdown.AddOptions(MakeDropdownList(windowedDropdownText));
-            windowedDropdown.onValueChanged.AddListener(delegate
+            windowedDropdown.AddOptions(windowedDropdownText);
+            windowedDropdown.onValueChanged.AddListener(SetFullscreen);
+            windowedDropdown.value = GetFullScreenModeDropdownIndex(currentFullscreen);
+            windowedDropdown.RefreshShownValue();
+        }
+
+        private void SetupResolutionDropdown()
+        {
+            List<string> options = new List<string>();
+            int currentResolutionIndex = 0;
+            for (int i = 0; i < resolutions.Length; i++)
             {
-                UpdateDisplayInfo();
-            });
+                string option = resolutions[i].width + " x " + resolutions[i].height;
+                options.Add(option);
+                if (Mathf.Approximately(resolutions[i].width, currentResolution.width) && Mathf.Approximately(resolutions[i].height, currentResolution.height))
+                {
+                    currentResolutionIndex = i;
+                }
+            }
 
             resolutionDropdown.ClearOptions();
-            resolutionDropdown.AddOptions(MakeDropdownList(resolutionDropdownText));
-            resolutionDropdown.onValueChanged.AddListener(delegate
-            {
-                UpdateDisplayInfo();
-            });
+            resolutionDropdown.AddOptions(options);
+            resolutionDropdown.onValueChanged.AddListener(SetResolution);
+            resolutionDropdown.value = currentResolutionIndex;
+            resolutionDropdown.RefreshShownValue();
+        }
+
+        private void LoadSettings()
+        {
+            currentResolution = new Resolution();
+            currentResolution.width = PlayerPrefs.GetInt(resolutionWidthPlayerPrefKey, Screen.currentResolution.width);
+            currentResolution.height = PlayerPrefs.GetInt(resolutionHeightPlayerPrefKey, Screen.currentResolution.height);
+            currentResolution.refreshRate = PlayerPrefs.GetInt(resolutionRefreshRatePlayerPrefKey, Screen.currentResolution.refreshRate);
+            currentFullscreen = (FullScreenMode)PlayerPrefs.GetInt(fullScreenPlayerPrefKey, (int)Screen.fullScreenMode);
+
+            UpdateDisplayInfo();
+        }
+
+        public void SetResolution(int resolutionIndex)
+        {
+            currentResolution = resolutions[resolutionIndex];
+            UpdateDisplayInfo();
+        }
+
+        public void SetFullscreen(int fullscreenIndex)
+        {
+            currentFullscreen = GetFullScreenMode(windowedDropdown.options[windowedDropdown.value].text.Trim());
+            UpdateDisplayInfo();
         }
 
         public void UpdateDisplayInfo()
         {
-            string selectedResolution = resolutionDropdown.options[resolutionDropdown.value].text.Trim();
-            FullScreenMode mode = GetFullScreenMode(windowedDropdown.options[windowedDropdown.value].text.Trim());
-
-            Match resolutionParse = Regex.Match(selectedResolution, resolutionParser);
-
-            int resX = int.Parse(resolutionParse.Groups[1].Value);
-            int resY = int.Parse(resolutionParse.Groups[2].Value);
-            if (int.TryParse(resolutionParse.Groups[4].Value, out int update))
-            {
 #if UNITY_EDITOR
-                UnityEngine.Debug.Log($"Setting {mode} with resolution {resX}x{resY} and update {update}");
+            UnityEngine.Debug.Log($"Setting {currentFullscreen} with resolution {currentResolution.width}x{currentResolution.height} * {currentResolution.refreshRate}");
 #else
-                Screen.SetResolution(resX, resY, mode, update);
+            Screen.SetResolution(currentResolution.width, currentResolution.height, currentFullscreen, currentResolution.refreshRate);
+            PlayerPrefs.SetInt(resolutionWidthPlayerPrefKey, currentResolution.width);
+            PlayerPrefs.SetInt(resolutionHeightPlayerPrefKey, currentResolution.height);
+            PlayerPrefs.SetInt(resolutionRefreshRatePlayerPrefKey, currentResolution.refreshRate);
+            PlayerPrefs.SetInt(fullScreenPlayerPrefKey, (int)currentFullscreen);
 #endif
-            }
-            else
-            {
-#if UNITY_EDITOR
-                UnityEngine.Debug.Log($"Setting {mode} with resolution {resX}x{resY}");
-#else
-                Screen.SetResolution(resX, resY, mode);
-#endif
-            }
         }
     }
 }
